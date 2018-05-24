c39a97b660767f52692a75a25ff65663b01edba3
Rogavactive <droga16@freeuni.edu.ge>
2018-05-24 15:03:16 +0400

    password check

--- Chess/WebContent/js/register_socket.js
+++ Chess/WebContent/js/register_socket.js
@@ -7,23 +7,55 @@ function enableBtn(){
 }
 
 function checkLen(password){
-    return password.length>7
+    var res = password.length > 7
+    setLenP(res)
+    return res
 }
 
-function checkASCII(password){
-    return /^[\x20-\x7E]*$/.test(password)
+function checkASCII(password) {
+    var res = /^[\x20-\x7E]*$/.test(password)
+    setASCIIP(res)
+    return res
 }
 
 function checkNumAndLetter(password){
-    return /\d/.test(password) && (/[a-z]/i.test(password) || /[A-Z]/i.test(password))
+    var res = /\d/.test(password) && (/[a-z]/i.test(password) || /[A-Z]/i.test(password))
+    setNumAndLetterP(res)
+    return res
 }
 
 function checkSimiliarity(password){
-    return !(password==document.getElementById("username-input").value)
+    var res = !(password==document.getElementById("username-input").value)
+    setSimilarityP(res)
+    return res
+}
+
+function setHintById(condition, hint_text, hint_id){
+    var inner_html = ""
+    if(condition)
+        inner_html = "<i class=\"material-icons hint-icons hint-accept\">check</i>"
+    else
+        inner_html = "<i class=\"material-icons hint-icons hint-reject\">close</i>"
+    document.getElementById(hint_id).innerHTML = inner_html + hint_text
+}
+
+function setLenP(condition){
+    setHintById(condition,"Minimum length: 8 characters.","pass-hint-length")
+}
+
+function setASCIIP(condition){
+    setHintById(condition,"Use english characters and numbers.","pass-hint-ascii")
+}
+
+function setNumAndLetterP(condition){
+    setHintById(condition,"At least one number and letter","pass-hint-numletter")
+}
+
+function setSimilarityP(condition){
+    setHintById(condition,"Pssword should differ from username.","pass-hint-similarity")
 }
 
 function checkPass(password){
-    //this will change DOM. a little board that indicates whats wrong.
     if(password.length==0){
         enableBtn()
         return
@@ -48,12 +80,18 @@ function checkPass(password){
 }
 
 window.onload = function(){
+
     document.getElementById("password-input").addEventListener('input', function(evt){
         checkPass(this.value)
     });
-    // document.getElementById("register-btn").onclick = function(){
-    //     document.getElementById("test-paragraph").innerHTML = "Button clicked!"
-    // }
+    document.getElementById("password-input").addEventListener('focus', function(evt){
+        document.getElementById("register-box-id").style.height = "500px"
+        document.getElementById("pass-hint-container").style.display = "block"
+        setLenP(false)
+        setASCIIP(false)
+        setNumAndLetterP(false)
+        setSimilarityP(false)
+    });
 }
 
 
