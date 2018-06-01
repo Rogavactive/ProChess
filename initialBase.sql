c993e142bc1c4b61020fbad615b150af4d83ba92
Rogavactive <droga16@freeuni.edu.ge>
2018-06-01 20:04:54 +0400

    sql script added

---
+++ initialBase.sql
@@ -0,0 +1,38 @@
+drop database if exists accountsDB;
+create database accountsDB;
+USE accountsDB;
+
+DROP TABLE IF EXISTS accounts;
+-- remove table if it already exists and start from scratch
+
+CREATE TABLE accounts (
+	ID        INT auto_increment,
+	username  nvarchar(64),
+	pass_hash nvarchar(128),
+	email     nvarchar(64),
+	primary key (ID),
+	unique key (username),
+	unique key (email)
+);
+
+CREATE TABLE validations(
+	ID	INT auto_increment,
+    username	nvarchar(64),
+    pass_hash	nvarchar(128),
+    email		nvarchar(64),
+    code		nvarchar(64),
+    primary key	(ID),
+    unique key(username),
+    unique key(email)
+);
+
+insert into accounts (username, pass_hash, email) values
+    ("rogavactive", "rogavactive", "droga16@freeuni.edu.ge");
+    
+
+
+-- select count(ID) as count_matches from accounts
+-- 	where username = "aa" and pass_hash = "aa";
+
+select *
+from accounts;
