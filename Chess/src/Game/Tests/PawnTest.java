defb236647dbf9bebfbf43536bf917838d40c5d4
Tornike Grigalashvili <tornikegrigalashvili@gmail.com>
2018-06-01 15:06:50 +0400

    delete worspace.xml cache locally

--- Chess/src/Game/Tests/PawnTest.java
+++ Chess/src/Game/Tests/PawnTest.java
@@ -3,6 +3,7 @@ package Game.Model.Tests;
 import Accounting.Model.AccountManager;
 import Game.Model.Pieces.Pawn;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PawnTest extends AccountManager {
     @Test
