57f5e32f8c1081fb9f1b68ca3b98fbe6d90ed6e5
lashabuxo <socialman98>
2018-07-04 23:00:47 +0400

    Chess Board Design Added

--- web/game.jsp
+++ web/game.jsp
@@ -75,8 +75,95 @@
         </form>
     </div>
     <div class="chess-board-container-div">
-        <div class="chess-board-div">
-            <div  class="canvas-container" > </div>
+        <div class="container">
+
+
+
+            <div class="contentPane">
+
+                <table id="chess_board" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td id="70"></td>
+                        <td id="71"></td>
+                        <td id="72"></td>
+                        <td id="73"></td>
+                        <td id="74"></td>
+                        <td id="75"></td>
+                        <td id="76"></td>
+                        <td id="77"></td>
+                    </tr>
+                    <tr>
+                        <td id="60"></td>
+                        <td id="61"></td>
+                        <td id="62"></td>
+                        <td id="63"></td>
+                        <td id="64"></td>
+                        <td id="65"></td>
+                        <td id="66"></td>
+                        <td id="67"></td>
+                    </tr>
+                    <tr>
+                        <td id="50"></td>
+                        <td id="51"></td>
+                        <td id="52"></td>
+                        <td id="53"></td>
+                        <td id="54"></td>
+                        <td id="55"></td>
+                        <td id="56"></td>
+                        <td id="57"></td>
+                    </tr>
+                    <tr>
+                        <td id="40"></td>
+                        <td id="41"></td>
+                        <td id="42"></td>
+                        <td id="43"></td>
+                        <td id="44"></td>
+                        <td id="45"></td>
+                        <td id="46"></td>
+                        <td id="47"></td>
+                    </tr>
+                    <tr>
+                        <td id="30"></td>
+                        <td id="31"></td>
+                        <td id="32"></td>
+                        <td id="33"></td>
+                        <td id="34"></td>
+                        <td id="35"></td>
+                        <td id="36"></td>
+                        <td id="37"></td>
+                    </tr>
+                    <tr>
+                        <td id="20"></td>
+                        <td id="21"></td>
+                        <td id="22"></td>
+                        <td id="23"></td>
+                        <td id="24"></td>
+                        <td id="25"></td>
+                        <td id="26"></td>
+                        <td id="27"></td>
+                    </tr>
+                    <tr>
+                        <td id="10"></td>
+                        <td id="11"></td>
+                        <td id="12"></td>
+                        <td id="13"></td>
+                        <td id="14"></td>
+                        <td id="15"></td>
+                        <td id="16"></td>
+                        <td id="17"></td>
+                    </tr>
+                    <tr>
+                        <td id="00" ></td>
+                        <td id="01"></td>
+                        <td id="02"></td>
+                        <td id="03"></td>
+                        <td id="04"></td>
+                        <td id="05"></td>
+                        <td id="06"></td>
+                        <td id="07"></td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
 </div>
--- web/js/game.js
+++ web/js/game.js
@@ -1,11 +1,12 @@
 var gameWebSocket = null;
 var chatWebSocket = null;
-var canvas=null;
+var board=null;
 $(document).ready(function(){
     connectGame();
     connectChat();
-    canvas=$('#board');
 
+    board=$('#chess_board');
+    started();
 });
 
 //GAME SOCKET
@@ -19,6 +20,7 @@ function connectGame() {
         this.gameWebSocket.onmessage = function(event) {
             var msg = event.data;
             console.log('onmessage::' + JSON.stringify(msg, null, 4));
+
         }
         this.gameWebSocket.onclose = function(event) {
             console.log('onclose::' + JSON.stringify(event, null, 4));
@@ -29,6 +31,89 @@ function connectGame() {
     } catch (exception) {
         console.error(exception);
     }
+}
+var Blacks_Bottom_Positions = {
+    "00": "B_etli", "01": "B_mxedari", "02": "B_ku", "03": "B_mepe", "04": "B_dedopali", "05": "B_ku", "06": "B_mxedari", "07": "B_etli",
+    "10": "B_paiki", "11": "B_paiki", "12": "B_paiki", "13": "B_paiki", "14": "B_paiki", "15": "B_paiki", "16": "B_paiki", "17": "B_paiki",
+    "60": "W_paiki", "61": "W_paiki", "62": "W_paiki", "63": "W_paiki", "64": "W_paiki", "65": "W_paiki", "66": "W_paiki", "67": "W_paiki",
+    "70": "W_etli", "71": "W_mxedari", "72": "W_ku", "73": "W_mepe", "74": "W_dedopali", "75": "W_ku", "76": "W_mxedari", "77": "W_etli",
+}
+
+var Pieces_Ascii = {
+    "mepe": ["King", "&#9813;", "&#9819;"],
+    "dedopali": ["Queen", "&#9812;", "&#9818;"],
+    "ku": ["Bishop", "&#9815;", "&#9821;"],
+    "mxedari": ["Knight", "&#9816;", "&#9822;"],
+    "etli": ["Rook", "&#9814;", "&#9820;"],
+    "paiki": ["Pawn", "&#9817;", "&#9823;"]
+}
+
+function fill_Chessboard(BottomColor){
+    for(var position in Blacks_Bottom_Positions ){
+        // Split the string up to find out color and piece name of the current position
+        var piece = Blacks_Bottom_Positions[position].substring(2);
+        var color = Blacks_Bottom_Positions[position][0];
+        var ascii;
+
+        if (color==BottomColor){
+            ascii=Pieces_Ascii[piece][1];
+        } else {
+            ascii=Pieces_Ascii[piece][2];
+        }
+        document.getElementById(position).innerHTML = "<a href='#'>" +ascii + "</a>";
+
+    }
+}
+function clearChessboard(){
+    for (var i=0;i<8;i++){
+        for (var j=0;j<8;j++){
+            document.getElementById(i+''+j).innerHTML = "";
+        }
+    }
+}
+var lastCell=null;
+function changeCellColor(cell,color){
+    document.getElementById(cell).style.color = color;
+}
+function replacePieces(cell1,cell2){
+   var saveHtml=  document.getElementById(cell1).innerHTML;
+   console.log(cell1+' '+cell2);
+    document.getElementById(cell1).innerHTML=document.getElementById(cell2).innerHTML;
+    document.getElementById(cell2).innerHTML=saveHtml;
+}
+function clickOnCell(cell){
+    if (lastCell==null) {
+        lastCell = cell;
+        changeCellColor(cell,'yellow');
+    } else {
+        replacePieces(lastCell,cell);
+        changeCellColor(lastCell,'transparent');
+        lastCell=null;
+    }
+
+  //  document.getElementById(cell).parentNode.style.backgroundColor='red';
+  //  document.getElementById(cell).parentNode.parentNode.style.backgroundColor='red';
+  //  document.getElementById(cell).parentNode.parentNode.parentNode.style.backgroundColor='red';
+}
+
+function started(){
+    fill_Chessboard('B');
+    $(document).click(function(event) {
+        var text = event.target.parentNode.id;
+        if (text.length!=2) text=event.target.id;
+        console.log(text);
+        clickOnCell(text);
+
+    });
+    //clearChessboard();
+    // document.getElementById('00').innerHTML = "<a href='#'>" +"&#9820;" + "</a>";
+    //  console.log("2");
+}
+function getPieceMoves(){
+
+}
+function pieceMoved(){
+
 }
 
 function getGameStatus() {
@@ -110,5 +195,5 @@ function chatBtnClick() {
 }
 
 function makeMove() {
-    
+
 }
