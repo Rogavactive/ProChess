b09fd8fd0d4d8534c4e71e56d0c9abe39883df14
lashabuxo <socialman98>
2018-07-05 18:39:00 +0400

    ChessBoard changed

--- web/js/game.js
+++ web/js/game.js
@@ -1,37 +1,3 @@
-var gameWebSocket = null;
-var chatWebSocket = null;
-var board=null;
-$(document).ready(function(){
-    connectGame();
-    connectChat();
-
-    board=$('#chess_board');
-    started();
-});
-
-//GAME SOCKET
-
-function connectGame() {
-    try {
-        this.gameWebSocket = new WebSocket("ws://localhost:8080/game");
-        this.gameWebSocket.onopen = function(event) {
-            console.log('onopen::' + JSON.stringify(event, null, 4));
-        }
-        this.gameWebSocket.onmessage = function(event) {
-            var msg = event.data;
-            console.log('onmessage::' + JSON.stringify(msg, null, 4));
-
-        }
-        this.gameWebSocket.onclose = function(event) {
-            console.log('onclose::' + JSON.stringify(event, null, 4));
-        }
-        this.gameWebSocket.onerror = function(event) {
-            console.log('onerror::' + JSON.stringify(event, null, 4));
-        }
-    } catch (exception) {
-        console.error(exception);
-    }
-}
 var Blacks_Bottom_Positions = {
     "00": "B_etli", "01": "B_mxedari", "02": "B_ku", "03": "B_mepe", "04": "B_dedopali", "05": "B_ku", "06": "B_mxedari", "07": "B_etli",
     "10": "B_paiki", "11": "B_paiki", "12": "B_paiki", "13": "B_paiki", "14": "B_paiki", "15": "B_paiki", "16": "B_paiki", "17": "B_paiki",
@@ -46,6 +12,72 @@ var Pieces_Ascii = {
     "mxedari": ["Knight", "&#9816;", "&#9822;"],
     "etli": ["Rook", "&#9814;", "&#9820;"],
     "paiki": ["Pawn", "&#9817;", "&#9823;"]
+}
+var Player;
+var markColor='yellow';
+var validMoveColor='green';
+var BoardState='';
+var gameStarted=false;
+
+var gameWebSocket = null;
+var chatWebSocket = null;
+
+$(document).ready(function(){
+    connectGame();
+    connectChat();
+    BoardStateChanged("B60506040615161416252624263536343645464446555654566566646675767477150715276557657");
+});
+
+//GAME SOCKET
+
+function connectGame() {
+    try {
+        this.gameWebSocket = new WebSocket("ws://localhost:8080/game");
+        this.gameWebSocket.onopen = function(event) {
+            console.log('onopen::' + JSON.stringify(event, null, 4));
+        }
+
+        this.gameWebSocket.onmessage = function(event) {
+            var msg = event.data;
+            console.log('onmessage::' + JSON.stringify(msg, null, 4));
+            BoardStateChanged(msg);
+        }
+        this.gameWebSocket.onclose = function(event) {
+            console.log('onclose::' + JSON.stringify(event, null, 4));
+        }
+        this.gameWebSocket.onerror = function(event) {
+            console.log('onerror::' + JSON.stringify(event, null, 4));
+        }
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
+function getValidMovesForPiece(pos){
+    var moves=[];
+    for (var i=0;i<BoardState.length;i+=4){
+        var x=BoardState.substring(i,i+2);
+        var y=BoardState.substring(i+2,i+4);
+       // console.log(x+' '+y);
+        if (x==pos) moves.push(y);
+    }
+    return moves;
+}
+
+
+function BoardStateChanged(json){
+    Player=json[0];
+    if (Player=='B') {
+        for (var i = 1; i < json.length; i++) {
+            BoardState+= 7-json[i];
+        }
+    }
+    console.log(BoardState);
+
+    if (!gameStarted) {
+        startGame();
+        gameStarted=true;
+    }
 }
 
 function fill_Chessboard(BottomColor){
@@ -73,22 +105,46 @@ function clearChessboard(){
 }
 var lastCell=null;
 function changeCellColor(cell,color){
-    document.getElementById(cell).style.color = color;
+   // console.log(2+document.getElementById(cell).style.backgroundColor);
+    document.getElementById(cell).style.backgroundColor = color;
 }
-function replacePieces(cell1,cell2){
-   var saveHtml=  document.getElementById(cell1).innerHTML;
-   console.log(cell1+' '+cell2);
+function resetCellColors(){
+    for (var i=0;i<8;i++)
+        for (var j=0;j<8;j++)
+            changeCellColor(i+''+j,'');
+}
+function movePiece(cell1,cell2){
+    var saveHtml=  document.getElementById(cell1).innerHTML;
+    console.log(cell1+' '+cell2);
     document.getElementById(cell1).innerHTML=document.getElementById(cell2).innerHTML;
     document.getElementById(cell2).innerHTML=saveHtml;
+    //send to socket
+    this.gameWebSocket.send(cell1+cell2);
+    BoardState='';
 }
 function clickOnCell(cell){
     if (lastCell==null) {
+        var validMoves=getValidMovesForPiece(cell);
+
+        if (validMoves.length==0) return;
+        for (var i=0;i<validMoves.length;i++) {
+            changeCellColor(validMoves[i], validMoveColor)
+          //  console.log(validMoves[i]);
+        }
         lastCell = cell;
-        changeCellColor(cell,'yellow');
+        changeCellColor(cell,markColor);
     } else {
-        replacePieces(lastCell,cell);
-        changeCellColor(lastCell,'transparent');
+        var validMoves=getValidMovesForPiece(lastCell);
+        for (var i=0;i<validMoves.length;i++)
+            if (validMoves[i]==cell) {
+                movePiece(lastCell, cell);
+                resetCellColors();
+                lastCell = null;
+                return;
+            }
+        resetCellColors();
         lastCell=null;
+        clickOnCell(cell);
     }
 
   //  document.getElementById(cell).parentNode.style.backgroundColor='red';
@@ -96,8 +152,8 @@ function clickOnCell(cell){
   //  document.getElementById(cell).parentNode.parentNode.parentNode.style.backgroundColor='red';
 }
 
-function started(){
-    fill_Chessboard('B');
+function startGame(){
+    fill_Chessboard(Player);
     $(document).click(function(event) {
         var text = event.target.parentNode.id;
         if (text.length!=2) text=event.target.id;
