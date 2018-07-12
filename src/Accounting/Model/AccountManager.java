a4bdea8d416c063daeb8b0d64fea5a3e04cdb2d1
Davit Bezhanishvili <datobejanishvili@gmail.com>
2018-07-12 19:46:04 +0400

    change rating fixed

--- src/Accounting/Model/AccountManager.java
+++ src/Accounting/Model/AccountManager.java
@@ -423,7 +423,7 @@ public class AccountManager {
                 break;
                 default: return false;
         }
-        String sqlQueryStatement = "update account_stats set \""+gameType_str+"\"="+matches+" where acc_ID="+id+";";
+        String sqlQueryStatement = "update account_stats set " + gameType_str + " = "+ matches +" where acc_ID = " +id+";";
         return simpleExecuteUpdate(sqlQueryStatement);
     }
 
@@ -431,17 +431,17 @@ public class AccountManager {
         String gameType_str = "";
         switch (gameType){
             case 0:
-                gameType_str="bulletRank";
+                gameType_str = "bulletRank";
                 break;
             case 1:
-                gameType_str="blitzRank";
+                gameType_str = "blitzRank";
                 break;
             case 2:
-                gameType_str="classicalRank";
+                gameType_str = "classicalRank";
                 break;
             default: return false;
         }
-        String sqlQueryStatement = "update account_stats set \""+gameType_str+"\"="+rating+" where acc_ID="+id+";";
+        String sqlQueryStatement = "update account_stats set " + gameType_str + " = " + rating + " where acc_ID = "+ id +";";
         return simpleExecuteUpdate(sqlQueryStatement);
     }
 
